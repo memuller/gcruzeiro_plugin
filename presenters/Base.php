dc2381f74f754d924a5bd15f5fe474d4ce1516be
Matheus E. Muller <hello@memuller.com>
2015-04-17 10:12:53 -0300

    Adds basic files.

---
+++ presenters/Base.php
@@ -0,0 +1,24 @@
+<?php
+	namespace Atlas\Presenters ;
+	use Presenter ;
+
+	class Base extends Presenter {
+
+		static $styles = array(
+			
+		);
+
+		static $scripts = array(
+
+		);
+
+		static $includes = array(
+		);
+
+
+		static function build(){
+			$presenter = get_called_class();
+			parent::build();
+		}
+	}
+?>
